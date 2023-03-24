86204e7fa6fd590f00218f0163bdc9381a970f3f
ocsoares <casoares016@gmail.com>
2023-03-24 18:50:56 -0300

    refactor: change transfer authorizer API link

--- src/providers/transfer-authorizer/transfer-authorizer.service.ts
+++ src/providers/transfer-authorizer/transfer-authorizer.service.ts
@@ -5,7 +5,9 @@ import axios from 'axios';
 @Injectable()
 export class TransferAuthorizerService implements IProvider {
     async execute(): Promise<string> {
-        const response = await axios.get('http://o4d9z.mocklab.io/notify');
+        const response = await axios.get(
+            'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6',
+        );
 
         return response.data.message;
     }
