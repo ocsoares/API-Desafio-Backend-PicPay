ce2c30ca85227fac0ee232ce8a4aedae44bf75a1
ocsoares <casoares016@gmail.com>
2023-03-29 19:55:29 -0300

    docs: add me user swagger docs

--- src/modules/user/use-cases/me-user/me-user.controller.ts
+++ src/modules/user/use-cases/me-user/me-user.controller.ts
@@ -1,4 +1,11 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
+import {
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiOkResponse,
+    ApiTags,
+    ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { IController, returnHandle } from '../../../../interfaces/IController';
 import { IReturnUser } from '../../../../interfaces/return-to-request/IReturnUser';
 import { CurrentUser } from '../../../../modules/auth/decorators/current-user.decorator';
@@ -8,6 +15,11 @@ import { MeUserService } from './me-user.service';
 export class MeUserController implements IController {
     constructor(private readonly _meUserService: MeUserService) {}
 
+    @ApiTags('auth-user')
+    @ApiBearerAuth()
+    @ApiUnauthorizedResponse()
+    @ApiBadRequestResponse()
+    @ApiOkResponse()
     @Get('me')
     @HttpCode(200)
     async handle(@CurrentUser() user: IReturnUser): Promise<returnHandle> {
